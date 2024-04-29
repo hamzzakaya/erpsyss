24fe761fa7852b4141c69dbc9badc1e523c7d3aa
hamza kaya <hamzakaya@hamza-MacBook-Air.local>
2024-04-29 20:46:57 +0300

    update product_code dropdown

--- src/pages/DataEntry.js
+++ src/pages/DataEntry.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import Select from "react-select";
+import axios from "axios";
 
 function DataEntry() {
   const [formData, setFormData] = useState({
@@ -12,6 +14,26 @@ function DataEntry() {
     productStatus: "",
     department: "",
   });
+
+  const [options, setOptions] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/get-products")
+      .then((response) => {
+        console.log("response", response);
+        const newOptions = response.data.documents.map((doc) => ({
+          value: doc.product_code,
+          label: `${doc.product_name} `,
+        }));
+        console.log(newOptions);
+        setOptions(newOptions);
+      })
+      .catch((error) => {
+        console.error("Error fetching products:", error);
+      });
+  }, []);
+
   const handleSaveProducts = () => {
     // Stok hesaplamaları yapılacak sayfaya yönlendirme
     // Örneğin React Router kullanarak /stock-page sayfasına yönlendirme yapılabilir
@@ -20,15 +42,13 @@ function DataEntry() {
     console.log("Ürünler kaydedildi:", productList);
     // Ürün listesini temizleme veya ilgili işlemler yapılabilir
     setProductList([]);
-};
-function formatDepartment(department) {
-  return department
-      .split('_')
-      .map(word => word.charAt(0).toUpperCase() + word.slice(1))
-      .join(' ');
-}
-
-
+  };
+  function formatDepartment(department) {
+    return department
+      .split("_")
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(" ");
+  }
 
   const [productList, setProductList] = useState([]);
 
@@ -40,59 +60,54 @@ function formatDepartment(department) {
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setFormData(prev => {
-        const newFormData = { ...prev, [name]: value };
+    setFormData((prev) => {
+      const newFormData = { ...prev, [name]: value };
 
-        if (name === "productCode") {
-            const product = products.find(p => p.productCode === value);
-            newFormData.productName = product ? product.productName : "";
-        } else if (name === "productName") {
-            const product = products.find(p => p.productName === value);
-            newFormData.productCode = product ? product.productCode : "";
-        }
+      if (name === "productCode") {
+        const product = products.find((p) => p.productCode === value);
+        newFormData.productName = product ? product.productName : "";
+      } else if (name === "productName") {
+        const product = products.find((p) => p.productName === value);
+        newFormData.productCode = product ? product.productCode : "";
+      }
 
-        if (name === "producedQuantity" || name === "scrap") {
-            const producedQuantity = parseInt(newFormData.producedQuantity, 10) || 0;
-            const scrap = parseInt(newFormData.scrap, 10) || 0;
-            newFormData.grossTotal = (producedQuantity + scrap).toString();
-        }
+      if (name === "producedQuantity" || name === "scrap") {
+        const producedQuantity =
+          parseInt(newFormData.producedQuantity, 10) || 0;
+        const scrap = parseInt(newFormData.scrap, 10) || 0;
+        newFormData.grossTotal = (producedQuantity + scrap).toString();
+      }
 
-        return newFormData;
+      return newFormData;
     });
-};
-
-
+  };
 
   const handleAddProduct = (e) => {
     e.preventDefault();
     setProductList([...productList, formData]); // Ürün listesine yeni ürün ekleyin
     // Sadece bazı alanları sıfırla, diğerlerini koru
-    setFormData(prevFormData => ({
-        ...prevFormData,
-        productName: "", // Ürün adını sıfırla
-        productCode: "", // Ürün kodunu sıfırla
-        producedQuantity: "", // Üretilen miktarı sıfırla
-        scrap: "", // Zayiatı sıfırla
-        grossTotal: "", // Brüt/Toplam değerini sıfırla
-        // Vardiya, Üretim Tarihi ve Durum değerlerini koru
-        // shift, productionDate, productStatus değerleri sabit kalacak
-        department: "",
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      productName: "", // Ürün adını sıfırla
+      productCode: "", // Ürün kodunu sıfırla
+      producedQuantity: "", // Üretilen miktarı sıfırla
+      scrap: "", // Zayiatı sıfırla
+      grossTotal: "", // Brüt/Toplam değerini sıfırla
+      // Vardiya, Üretim Tarihi ve Durum değerlerini koru
+      // shift, productionDate, productStatus değerleri sabit kalacak
+      department: "",
     }));
-};
+  };
 
+  const handleRemoveProduct = (index) => {
+    setProductList((currentList) => currentList.filter((_, i) => i !== index));
+  };
 
-const handleRemoveProduct = index => {
-  setProductList(currentList => currentList.filter((_, i) => i !== index));
-};
-
-
-const handleProductsSave = () => {
-  localStorage.setItem('producedProducts', JSON.stringify(productList));
-  console.log("Ürünler kaydedildi:", productList);
-  setProductList([]);
-};
-
-
+  const handleProductsSave = () => {
+    localStorage.setItem("producedProducts", JSON.stringify(productList));
+    console.log("Ürünler kaydedildi:", productList);
+    setProductList([]);
+  };
 
   return (
     <div className="p-5">
@@ -101,22 +116,19 @@ const handleProductsSave = () => {
         onSubmit={handleAddProduct}
         className="flex flex-wrap justify-start items-center gap-4"
       >
-        <input
-          type="text"
-          name="productName"
-          placeholder="Ürün Adı"
-          value={formData.productName}
-          onChange={handleChange}
-          className="flex-grow min-w-0 w-1/6 p-2 rounded border"
-        />
-        <input
-          type="text"
-          name="productCode"
-          placeholder="Ürün Kodu"
-          value={formData.productCode}
-          onChange={handleChange}
-          className="flex-grow min-w-0 w-1/6 p-2 rounded border"
-        />
+        <div>
+          <Select
+            onChange={(e) => {
+              console.log(e);
+              setFormData({
+                ...formData,
+                productCode: e.value,
+                productName: e.label,
+              });
+            }}
+            options={options}
+          />
+        </div>
         <input
           type="number"
           name="producedQuantity"
@@ -171,16 +183,16 @@ const handleProductsSave = () => {
           <option value="bisküvi ürün">Bisküvi Ürün</option>
         </select>
         <select
-                    name="department"
-                    value={formData.department}
-                    onChange={handleChange}
-                    className="flex-grow min-w-0 w-1/6 p-2 rounded border"
-                >
-                    <option value="">Bölüm Seç</option>
-                    <option value="fincan_torna">Fincan Torna</option>
-                    <option value="tabak_torna">Tabak Torna</option>
-                    <option value="izostatik">İzostatik</option>
-                </select>
+          name="department"
+          value={formData.department}
+          onChange={handleChange}
+          className="flex-grow min-w-0 w-1/6 p-2 rounded border"
+        >
+          <option value="">Bölüm Seç</option>
+          <option value="fincan_torna">Fincan Torna</option>
+          <option value="tabak_torna">Tabak Torna</option>
+          <option value="izostatik">İzostatik</option>
+        </select>
         <button
           type="submit"
           className="bg-blue-500 hover:bg-blue-700 text-white p-2 rounded"
@@ -197,21 +209,39 @@ const handleProductsSave = () => {
               Üretilen Miktar: {product.producedQuantity}, Zayiat:{" "}
               {product.scrap}, Brüt/Toplam: {product.grossTotal}, Vardiya:{" "}
               {product.shift}, Üretildiği Tarih: {product.productionDate}, Ürün
-              Durumu: {product.productStatus}, Bölüm: {formatDepartment(product.department)}
-              <button onClick={() => handleRemoveProduct(index)} className="ml-4 bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-3 rounded">
+              Durumu: {product.productStatus}, Bölüm:{" "}
+              {formatDepartment(product.department)}
+              <button
+                onClick={() => handleRemoveProduct(index)}
+                className="ml-4 bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-3 rounded"
+              >
                 ×
               </button>
-              
             </li>
           ))}
         </ul>
-        <button onClick={handleProductsSave} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-    Kaydet
-</button>
 
+        <button
+          onClick={handleProductsSave}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Kaydet
+        </button>
       </div>
     </div>
   );
 }
 
 export default DataEntry;
+
+// axios.get(http://localhost:8080/get-products)
+// .then(response => {
+//   const product = response;
+//
+//   setTemperature(temperatureCelsius.toFixed(0));
+// })
+// .catch(error => {
+//   console.log(error);
+//   setTemperature('Hava durumu bilgileri alınamadı.');
+// });
+// };
--- src/pages/ProductManagement.js
+++ src/pages/ProductManagement.js
@@ -1,36 +1,50 @@
 // src/pages/ProductManagement.js
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
+import Select from "react-select";
 
 function ProductManagement() {
-  const [productName, setProductName] = useState('');
-  const [productCode, setProductCode] = useState('');
+  const [productName, setProductName] = useState("");
+  const [productCode, setProductCode] = useState("");
   const [products, setProducts] = useState(() => {
-    const savedProducts = localStorage.getItem('products');
+    const savedProducts = localStorage.getItem("products");
     return savedProducts ? JSON.parse(savedProducts) : [];
   });
 
+  const [selectedOption, setSelectedOption] = useState(null);
+
+  const options = [
+    { value: "chocolate", label: "Chocolate 12312s1212s" },
+    { value: "strawberry", label: "Strawberry" },
+    { value: "vanilla", label: "Vanilla" },
+  ];
+
   useEffect(() => {
-    localStorage.setItem('products', JSON.stringify(products));
+    localStorage.setItem("products", JSON.stringify(products));
   }, [products]);
 
   const handleAddProduct = (e) => {
     e.preventDefault();
     if (!productName || !productCode) {
-      alert('Her iki alan da doldurulmalıdır.');
+      alert("Her iki alan da doldurulmalıdır.");
       return;
     }
     const newProduct = { productName, productCode };
-    setProducts(prevProducts => [...prevProducts, newProduct]);
-    setProductName('');
-    setProductCode('');
+    setProducts((prevProducts) => [...prevProducts, newProduct]);
+    setProductName("");
+    setProductCode("");
   };
 
   return (
     <div>
-      <h1 className="text-lg font-semibold mb-4 text-blue-700">Ürün Tanımlama</h1>
+      <h1 className="text-lg font-semibold mb-4 text-blue-700">
+        Ürün Tanımlama
+      </h1>
       <form onSubmit={handleAddProduct}>
         <div className="mb-4">
-          <label htmlFor="productName" className="block text-gray-700 text-sm font-bold mb-2">
+          <label
+            htmlFor="productName"
+            className="block text-gray-700 text-sm font-bold mb-2"
+          >
             Ürün Adı
           </label>
           <input
@@ -42,7 +56,10 @@ function ProductManagement() {
           />
         </div>
         <div className="mb-4">
-          <label htmlFor="productCode" className="block text-gray-700 text-sm font-bold mb-2">
+          <label
+            htmlFor="productCode"
+            className="block text-gray-700 text-sm font-bold mb-2"
+          >
             Ürün Kodu
           </label>
           <input
@@ -53,7 +70,17 @@ function ProductManagement() {
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           />
         </div>
-        <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+        <div className="App">
+          <Select
+            defaultValue={selectedOption}
+            onChange={setSelectedOption}
+            options={options}
+          />
+        </div>
+        <button
+          type="submit"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        >
           Ürün Ekle
         </button>
       </form>
